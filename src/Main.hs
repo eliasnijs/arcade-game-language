f293b354a9193b34b771d0221aed6222bc55046d
Felix Van der Jeugt <felix.vanderjeugt@posteo.net>
2021-11-16 13:29:42 +0100

    add I/O sample

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,9 @@
 import VoorbeeldModule (hoi)
 
+import System.Environment (getArgs)
+
 main :: IO ()
-main = putStrLn hoi
+main = do (filename:_) <- getArgs -- het eerste argument (stack run tetris.xyz)
+          contents <- readFile filename -- bevat de inhoud van tetris.xyz als String
+          putStrLn hoi
+          putStrLn contents
